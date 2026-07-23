e955a79f88a5c53fbc2439d693887d48b22c34aa
Michal Biesek <mbiesek@cribl.io>
2026-07-23 14:28:36 +0200

    Regenerate SDK 0.4.0

--- criblmgmtplane.go
+++ criblmgmtplane.go
@@ -131,9 +131,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *CriblMgmtPlane {
 	sdk := &CriblMgmtPlane{
-		SDKVersion: "0.4.0-rc.1",
+		SDKVersion: "0.4.0",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/go 0.4.0-rc.1 2.918.4 1.0 github.com/Cribl-Community/cribl-cloud-management-sdk-go",
+			UserAgent:  "speakeasy-sdk/go 0.4.0 2.918.4 1.0 github.com/Cribl-Community/cribl-cloud-management-sdk-go",
 			ServerList: ServerList,
 		},
 		hooks: hooks.New(),
